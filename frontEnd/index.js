19159e1b7f302d82702967f9fa74de289de9c4d8
vine-nam <wldkdkdk7@gmail.com>
2019-04-15 13:33:48 +0900

    on-click 함수 수정

--- frontEnd/index.js
+++ frontEnd/index.js
@@ -11,13 +11,23 @@ var editor = new Vue({
     info: null
   },
   methods: {
-    runBtn: function() {
+    getBtn: function() {
       axios
-        //.get('/DB',{ crossDomain: true })
+        .get('/DB')
+        .then(response => (
+          this.info = response.data.root
+          ), (error) => {
+            console.log(error)
+            this.info = error
+          }
+        )
+    },
+    postBtn: function() {
+      axios
         .post('/DB', {sql: this.query})
         .then(response => (
           this.info = response.data.root
-          ), (error) => { 
+          ), (error) => {
             console.log(error)
             this.info = error
           }
@@ -26,14 +36,18 @@ var editor = new Vue({
     defaultBtn: function() {
       this.query = 'SELECT * FROM TB_C_IFINFO '
     },
-    useFlagBtn: function() {
+    whereBtn: function(type) {
       var sqls = this.query.toUpperCase().split(' ')
       for (let i = 0; i < sqls.length; i++) {
         if(/(|\n)WHERE/.exec(sqls[i]) ) {
           var str = ''
           for (let j = 0; j < sqls.length; j++) {
             if(/(|\n)WHERE/.exec(sqls[j]) ) {
-              str += "\nWHERE USE_FLAG = 'Y' "
+                if(type === 'useFlag') {
+                    str += "\nWHERE USE_FLAG = 'Y' "
+                } else if(type === 'ifId') {
+                    str += "\nWHERE IF_ID = '' "
+                }
               if(j !== sqls.length-1 & sqls[j+1] !== "") {
                 str += "\n  AND "
               }
@@ -45,7 +59,11 @@ var editor = new Vue({
           }
         } else {
           if(i === sqls.length-1) {
-            this.query = this.query + "\nWHERE USE_FLAG = 'Y'"
+            if(type === 'useFlag') {
+                this.query = this.query + "\nWHERE USE_FLAG = 'Y' "
+            } else if(type === 'ifId') {
+                this.query = this.query + "\nWHERE IF_ID = '' "
+            }
           }
         }
       }
