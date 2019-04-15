d33ce664a06c425ff181e5ce58c4a4b39e492a3d
vine-nam <wldkdkdk7@gmail.com>
2019-04-15 17:48:09 +0900

    time() 추가

--- frontEnd/index.js
+++ frontEnd/index.js
@@ -4,21 +4,26 @@
 
 axios.defaults.baseURL = 'http://localhost:3001';
 
+function time() {
+    var date = new Date();
+    return "[" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "]   ";
+}
+
 var editor = new Vue({
   el: '#editor',
   data: {
-    query: 'SELECT * FROM TB_C_IFINFO ',
-    info: null
+    query: "SELECT * FROM TB_C_IFINFO \nWHERE USE_FLAG = 'Y' ",
+    info: ''
   },
   methods: {
     getBtn: function() {
       axios
-        .get('/DB')
+        .get('/DB', {  params: {sql: this.query} })
         .then(response => (
           this.info = response.data.root
           ), (error) => {
-            console.log(error)
-            this.info = error
+            console.log(error);
+            this.info = time() +  error + "\n" + this.info
           }
         )
     },
@@ -28,25 +33,27 @@ var editor = new Vue({
         .then(response => (
           this.info = response.data.root
           ), (error) => {
-            console.log(error)
-            this.info = error
+            console.log(error);
+            this.info = time() +  error + "\n" + this.info
           }
         )
     },
     defaultBtn: function() {
-      this.query = 'SELECT * FROM TB_C_IFINFO '
+      this.query = "SELECT * FROM TB_C_IFINFO \nWHERE USE_FLAG = 'Y' "
     },
     whereBtn: function(type) {
-      var sqls = this.query.toUpperCase().split(' ')
+      var sqls = this.query.toUpperCase().split(' ');
       for (let i = 0; i < sqls.length; i++) {
         if(/(|\n)WHERE/.exec(sqls[i]) ) {
           var str = ''
           for (let j = 0; j < sqls.length; j++) {
             if(/(|\n)WHERE/.exec(sqls[j]) ) {
-                if(type === 'useFlag') {
+                if(type === 'U') {
                     str += "\nWHERE USE_FLAG = 'Y' "
-                } else if(type === 'ifId') {
+                } else if(type === 'I') {
                     str += "\nWHERE IF_ID = '' "
+                } else if(type === 'W') {
+                    str += "\nWHERE WORK_GROUP = '' "
                 }
               if(j !== sqls.length-1 & sqls[j+1] !== "") {
                 str += "\n  AND "
@@ -59,10 +66,12 @@ var editor = new Vue({
           }
         } else {
           if(i === sqls.length-1) {
-            if(type === 'useFlag') {
+            if(type === 'U') {
                 this.query = this.query + "\nWHERE USE_FLAG = 'Y' "
-            } else if(type === 'ifId') {
+            } else if(type === 'I') {
                 this.query = this.query + "\nWHERE IF_ID = '' "
+            } else if(type === 'W') {
+                 this.query = this.query + "\nWHERE WORK_GROUP = '' "
             }
           }
         }
@@ -70,4 +79,3 @@ var editor = new Vue({
     }
   }
 })
-
